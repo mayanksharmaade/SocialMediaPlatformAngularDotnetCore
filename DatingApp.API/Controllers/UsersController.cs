4d4ff0b4d3cf81d35e29987c7dec10b80261f93f
mayankcst <mayankcst@gmail.com>
2025-03-10 07:54:54 +0530

    Add project files.

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public UsersController( DataContext context)
+        {
+            this.dataContext = context;
+        }
+        [HttpGet]
+        public  async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users =  await dataContext.Users.ToListAsync();
+
+            if (users.Count == 0)
+            { return NotFound(users); }
+            else
+                
+                    return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task< ActionResult<IEnumerable<AppUser>> >GetUsers( int id)
+        {
+            var user =  await dataContext.Users.Where(x=>x.ID==id).FirstAsync();
+
+            if ( user== null)
+            { return NotFound(user); }
+            else
+                return Ok(user);
+        }
+
+       
+    }
+}
---
+++ DatingApp.API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Entities
+{
+    public class AppUser
+    {
+        [Required]
+        public  Int32 ID { get; set; }
+        public required string UserName { get; set; }
+    }
+}
